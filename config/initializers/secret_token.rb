c840ec0cfdb60f756e99f316a77c6370a72e0c97
sean <sean@parsnip.(none)>
2012-12-09 16:59:22 -0500

    barebones mongodb

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GuitarTab::Application.config.secret_token = '16e714ae14e471216ae22e0b85729aa1936f1207da94f52716cdbb8bf8abf28b2658a072cba9001eaf435a9c7dcba37e8bce474662c435619bf10cf3b37c21c6'
