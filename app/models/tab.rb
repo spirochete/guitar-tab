14ae60b3e7b3e6878571636948e5744d4f46f769
sean <sean@parsnip.(none)>
2012-12-11 15:34:23 -0500

    added file attachments with carrierwave! still working out multipart

--- app/models/tab.rb
+++ app/models/tab.rb
@@ -1,12 +1,17 @@
 class Tab
   include MongoMapper::Document
+  include Sunspot::Rails
   include Sunspot::Mongo
+
+  attr_accessible :submission, :submission_cache
+  mount_uploader :submission, SubmissionUploader
 
   belongs_to :user
 
   key :artist, String
   key :song, String
-  key :tuning
+  key :tuning, String
+  key :submission_file_name, String
 
   searchable do
     string :artist
@@ -16,5 +21,6 @@ class Tab
     text :song
     text :tuning
     string :user_id
+    string :submission_file_name
   end
 end
